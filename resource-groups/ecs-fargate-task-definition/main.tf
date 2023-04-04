ecd59467a3135de229bfa1978a3e03e065e5aade
Mark Henwood <mcbhenwood@icloud.com>
2023-04-04 16:27:10 +0100

    Add resource group for common ECS task definition

---
+++ resource-groups/ecs-fargate-task-definition/main.tf
@@ -0,0 +1,71 @@
+resource "aws_ecs_task_definition" "task" {
+  family                = var.family_name
+  container_definitions = jsonencode([
+    {
+      name        = var.container_name
+      command     = var.override_command # If null, does not override Dockerfile original command
+      environment = var.container_environment_variables
+      essential   = true
+      healthCheck = var.container_healthcheck_command == null ? null : {
+        command     = ["CMD-SHELL", var.container_healthcheck_command]
+        startPeriod = 10
+        timeout     = 10
+      }
+
+      image            = var.image
+      logConfiguration = {
+        "logDriver" : "awslogs",
+        "options" : {
+          "awslogs-create-group" : "true",
+          "awslogs-group" : var.container_log_group_name,
+          "awslogs-region" : var.aws_region,
+          "awslogs-stream-prefix" : "execution"
+        }
+      }
+
+      mountPoints = [
+        for mount in var.efs_mounts :
+        {
+          containerPath : mount["mount_point"],
+          readOnly : mount["read_only"],
+          sourceVolume : mount["volume_name"]
+        }
+      ]
+
+      portMappings = var.container_port == null ? null : [
+        {
+          containerPort = var.container_port
+        }
+      ]
+      secrets = var.secret_environment_variables
+    }
+  ])
+  cpu                      = var.container_cpu
+  execution_role_arn       = var.ecs_execution_role_arn
+  memory                   = var.container_memory
+  network_mode             = "awsvpc" # Fixed for Fargate
+  requires_compatibilities = ["FARGATE"]
+  runtime_platform {
+    cpu_architecture        = "X86_64"
+    operating_system_family = "LINUX"
+  }
+  task_role_arn = aws_iam_role.task_role.arn
+
+  dynamic "volume" {
+    for_each = var.efs_mounts
+    iterator = mount
+
+    content {
+      efs_volume_configuration {
+        authorization_config {
+          access_point_id = mount.value["access_point_id"]
+          iam             = "DISABLED"
+        }
+        file_system_id     = mount.value["file_system_id"]
+        transit_encryption = "ENABLED"
+      }
+
+      name = mount.value["volume_name"]
+    }
+  }
+}
