78f2f15eb2bb561eb4ea8aeea93302b4e70d87bf
Mark Henwood <mcbhenwood@icloud.com>
2023-05-03 13:49:30 +0100

    Add resource group for common RDS PostgreSQL definition

---
+++ resource-groups/rds-postgres/main.tf
@@ -0,0 +1,67 @@
+resource "aws_db_subnet_group" "subnet_group" {
+  name       = var.db_name
+  subnet_ids = var.subnet_ids
+}
+
+resource "aws_db_instance" "db" {
+  allocated_storage               = var.allocated_storage_gb
+  allow_major_version_upgrade     = false
+  backup_retention_period         = var.backup_retention_period_days
+  db_name                         = var.db_name # NB Postgres db names use underscores, not hyphens
+  db_subnet_group_name            = aws_db_subnet_group.subnet_group.name
+  enabled_cloudwatch_logs_exports = ["postgresql"]
+  engine                          = "postgres"
+  engine_version                  = var.postgres_engine_version
+  identifier                      = var.db_name # NB RDS identifiers use hyphens, not underscores
+  instance_class                  = var.db_instance_class
+  multi_az                        = true
+  password                        = var.db_password
+  port                            = var.postgres_port
+  publicly_accessible             = false
+  skip_final_snapshot             = var.skip_final_snapshot
+  storage_encrypted               = true
+  username                        = var.db_username
+  vpc_security_group_ids          = [aws_security_group.db.id]
+}
+
+resource "aws_security_group" "db" {
+  name        = "${var.db_name}-db"  # TODO rename
+  description = "RDS ${var.db_name} DB"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "${var.db_name}-db"
+  }
+}
+
+resource "aws_security_group" "db_clients" {
+  name        = "${var.db_name}-db-clients"  # TODO rename
+  description = "Entities permitted to access the ${var.db_name} database"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "${var.db_name}-db-clients"
+  }
+}
+
+resource "aws_security_group_rule" "db_postgres_in" {
+  security_group_id = aws_security_group.db.id
+  description       = "Allow ${var.postgres_port} inwards from db-clients SG"
+
+  from_port                = var.postgres_port
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.db_clients.id
+  to_port                  = var.postgres_port
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "db_client_postgres_out" {
+  security_group_id = aws_security_group.db_clients.id
+  description       = "Allow ${var.postgres_port} from to db"
+
+  from_port                = var.postgres_port
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.db.id
+  to_port                  = var.postgres_port
+  type                     = "egress"
+}
---
+++ resource-groups/rds-postgres/variables.tf
@@ -0,0 +1,62 @@
+variable "allocated_storage_gb" {
+  type        = number
+  description = "Storage allocation in GiB"
+  default     = 10
+}
+
+variable "backup_retention_period_days" {
+  type        = number
+  description = "Number of days for which to keep backups"
+  default     = 14
+}
+
+variable "db_instance_class" {
+  type        = string
+  description = "Type of DB instance"
+  default     = "db.t3.small"
+}
+
+variable "db_name" {
+  type        = string
+  description = "Short, no-spaces version of database name"
+}
+
+variable "db_password" {
+  type        = string
+  sensitive   = true
+  description = "Password for the database master user"
+}
+
+variable "db_username" {
+  type        = string
+  description = "Username for master user"
+}
+
+variable "postgres_engine_version" {
+  type        = string
+  description = "Version number of db engine to use"
+  default     = "14.6" # TODO Determine version currently in use for DMP 1.0
+}
+
+variable "postgres_port" {
+  type        = number
+  description = "Port on which the DB should accept connections"
+  default     = 5432
+}
+
+# TODO It's beyond the scope of an Alpha to manage final snapshot preferences - Leaving this here as a reminder.
+variable "skip_final_snapshot" {
+  type        = bool
+  description = "Whether or not to allow the DB to be deleted without taking a snapshot"
+  default     = true
+}
+
+variable "subnet_ids" {
+  type        = list(string)
+  description = "List of IDs of subnets for DB subnet groups"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "ID of the VPC containing the service"
+}
