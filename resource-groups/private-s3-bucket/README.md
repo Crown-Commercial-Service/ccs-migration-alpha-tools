8aed2be6d36ed96d7fa49f750dbbbdfcf9260660
Mark Henwood <mcbhenwood@icloud.com>
2023-06-22 15:07:22 +0100

    Add resource group for S3 buckets

---
+++ resource-groups/private-s3-bucket/README.md
@@ -0,0 +1,10 @@
+# Private S3 Bucket
+
+Resources to reduce repetiion in definition of a bucket with common access requirements.
+
+Resources include:
+
+* The bucket definition
+* Versioning control
+* Encryption config
+* IAM policy JSON for reading / writing / deleting objects
---
+++ resource-groups/private-s3-bucket/variables.tf
@@ -0,0 +1,16 @@
+variable "bucket_name" {
+  type        = string
+  description = "Name to give the S3 bucket (must be globally unique)"
+}
+
+variable "is_ephemeral" {
+  type        = bool
+  description = "If set to true, indicates that this module is expected to be destroyed as a matter of course (so will set `force_destroy` on aws_s3_bucket resources)"
+  default     = false
+}
+
+variable "versioning" {
+  type        = bool
+  description = "If set to true, will activate versioning on all objects placed in the bucket"
+  default     = false
+}
