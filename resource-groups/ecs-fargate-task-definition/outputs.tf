75d16d7e760cfe8cf59946583973d47d0ba3b072
Mark Henwood <mcbhenwood@icloud.com>
2023-07-04 15:35:56 +0100

    Add task family name to module output

--- resource-groups/ecs-fargate-task-definition/outputs.tf
+++ resource-groups/ecs-fargate-task-definition/outputs.tf
@@ -8,6 +8,11 @@ output "task_definition_arn" {
   value       = aws_ecs_task_definition.task.arn
 }
 
+output "task_family_name" {
+  description = "Family name for the task"
+  value       = aws_ecs_task_definition.task.family
+}
+
 output "task_role_arn" {
   description = "ARN of the IAM role assigned to all tasks run under this task definition"
   value       = aws_iam_role.task_role.arn
