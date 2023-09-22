a0642b5eba0e2a51426aada826e218cdee338bd2
Andrew Hemming <drewhemm@users.noreply.github.com>
2023-09-22 07:45:01 +0100

    Update outputs.tf

--- resource-groups/acm-certificate/outputs.tf
+++ resource-groups/acm-certificate/outputs.tf
@@ -1,4 +1,4 @@
 output "certificate_arn" {
   description = "ARN of the certificate created"
-  value       = aws_acm_certificate_validation.cert.arn
+  value       = aws_acm_certificate_validation.validation.arn
 }
