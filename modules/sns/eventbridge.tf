435b2126b07f64d1b729b01bcb2a450441eb209c
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2023-10-31 11:52:19 +0000

    Revert changes with the provider name

--- modules/sns/eventbridge.tf
+++ modules/sns/eventbridge.tf
@@ -1,6 +1,6 @@
 # us-east-1
 resource "aws_cloudwatch_event_rule" "route53_create_hosted_zone_us" {
-  provider = providers.aws.global-service-region
+  provider = aws.global-service-region
 
   name        = "route53-create-hosted-zone-rule-us"
   description = "Capture new Route 53 hosted zone event when it is created in us-east-1"
@@ -25,7 +25,7 @@ resource "aws_cloudwatch_event_rule" "route53_create_hosted_zone_us" {
 
 # us-east-1 needs to target the default event bus in eu-west-2
 resource "aws_cloudwatch_event_target" "target_eu" {
-  provider = providers.aws.global-service-region
+  provider = aws.global-service-region
 
   rule = aws_cloudwatch_event_rule.route53_create_hosted_zone_us.name
   arn  = aws_cloudwatch_event_rule.route53_create_hosted_zone_eu.arn
