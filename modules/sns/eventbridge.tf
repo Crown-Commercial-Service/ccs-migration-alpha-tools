c354ceaf1a203d8ebf7e17a07914b604e4a480de
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2023-10-31 16:09:23 +0000

    Test required providers

--- modules/sns/eventbridge.tf
+++ modules/sns/eventbridge.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
-    default-region        = {}
-    global-service-region = {}
+    aws.default-region        = {}
+    aws.global-service-region = {}
 
   }
 }
 
 # us-east-1
 resource "aws_cloudwatch_event_rule" "route53_create_hosted_zone_us" {
-  provider = global-service-region
+  provider = aws.global-service-region
 
   name        = "route53-create-hosted-zone-rule-us"
   description = "Capture new Route 53 hosted zone event when it is created in us-east-1"
@@ -33,7 +33,7 @@ resource "aws_cloudwatch_event_rule" "route53_create_hosted_zone_us" {
 
 # us-east-1 needs to target the default event bus in eu-west-2
 resource "aws_cloudwatch_event_target" "target_eu" {
-  provider = global-service-region
+  provider = aws.global-service-region
 
   rule = aws_cloudwatch_event_rule.route53_create_hosted_zone_us.name
   arn  = aws_cloudwatch_event_rule.route53_create_hosted_zone_eu.arn
@@ -41,7 +41,7 @@ resource "aws_cloudwatch_event_target" "target_eu" {
 
 # Event rule in eu-west-2 to capture the forwarded event
 resource "aws_cloudwatch_event_rule" "route53_create_hosted_zone_eu" {
-  provider    = default-region
+  provider    = aws.default-region
   name        = "route53-create-hosted-zone-rule-eu"
   description = "Capture Route 53 hosted zone event"
 
@@ -65,7 +65,7 @@ resource "aws_cloudwatch_event_rule" "route53_create_hosted_zone_eu" {
 
 # Target for the eu-west-2 Event rule to invoke the Lambda function
 resource "aws_cloudwatch_event_target" "lambda_function_in_eu" {
-  provider = default-region
+  provider = aws.default-region
   rule     = aws_cloudwatch_event_rule.route53_create_hosted_zone_eu.name
   arn      = aws_lambda_function.route53_notifier.arn
 }
