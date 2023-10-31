40f8a223e987e560251ebe309cc61adf8f5baf0e
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2023-10-31 13:23:18 +0000

    Remove double quotes

--- modules/sns/main.tf
+++ modules/sns/main.tf
@@ -3,7 +3,7 @@ resource "aws_sns_topic" "route53_notifications" {
 }
 
 resource "aws_sns_topic_subscription" "email_subscription" {
-  topic_arn = "aws_sns_topic.route53_notifications.arn"
+  topic_arn = aws_sns_topic.route53_notifications.arn
   protocol  = "email"
   endpoint  = "andrew.hemming@redrockconsulting.co.uk"
 }
