8f8fcff8123b4ae797caab2fbeeef558bc3af544
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2023-11-01 16:22:18 +0000

    Add email subscription

--- modules/sns/main.tf
+++ modules/sns/main.tf
@@ -7,3 +7,9 @@ resource "aws_sns_topic_subscription" "email_subscription" {
   protocol  = "email"
   endpoint  = "andrew.hemming@redrockconsulting.co.uk"
 }
+
+# resource "aws_sns_topic_subscription" "email_subscription" {
+#   topic_arn = aws_sns_topic.route53_notifications.arn
+#   protocol  = "email"
+#   endpoint  = "internal-it@crowncommercial.gov.uk"
+# }
