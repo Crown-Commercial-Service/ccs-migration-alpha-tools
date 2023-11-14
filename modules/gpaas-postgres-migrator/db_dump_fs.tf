baf13a386de74904abeb018b43130ea4286a96c6
Andrew Hemming <drewhemm@users.noreply.github.com>
2023-11-14 17:15:10 +0000

    Conditional logic

--- modules/gpaas-postgres-migrator/db_dump_fs.tf
+++ modules/gpaas-postgres-migrator/db_dump_fs.tf
@@ -62,7 +62,7 @@ EOF
 }
 
 resource "aws_efs_mount_target" "db_dump" {
-  for_each        = var.efs_subnet_ids
+  for_each        = length(var.efs_subnet_ids) > 0 ? var.efs_subnet_ids : [var.subnet_id]
   file_system_id  = aws_efs_file_system.db_dump.id
   security_groups = [aws_security_group.db_dump_fs.id]
   subnet_id       = each.value
