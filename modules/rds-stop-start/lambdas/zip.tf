e719e740dacf635ddfbb4fd61d8f45d9481ca533
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2023-11-15 16:06:06 +0000

    Fix module path

--- modules/rds-stop-start/lambdas/zip.tf
+++ modules/rds-stop-start/lambdas/zip.tf
@@ -1,5 +1,5 @@
 data "archive_file" "start_stop_rds_function" {
   type        = "zip"
-  source_file = "${path.module}/lambdas/start_stop_rds.py"
-  output_path = "${path.module}/lambdas/start_stop_rds.zip"
+  source_file = "${path.module}/start_stop_rds.py"
+  output_path = "${path.module}/start_stop_rds.zip"
 }
