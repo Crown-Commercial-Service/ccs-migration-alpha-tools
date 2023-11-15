5e63c6457e248aec87daa2d9a4e12efd2a76cbb5
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2023-11-15 09:51:15 +0000

    Tf fmt

--- modules/rds-stop-start/lambdas/scheduler_iam_role.tf
+++ modules/rds-stop-start/lambdas/scheduler_iam_role.tf
@@ -2,28 +2,28 @@ resource "aws_iam_role" "eventbridge_scheduler_role" {
   name = "eventbridge-scheduler-role"
   assume_role_policy = jsondecode(
     {
-    "Version": "2012-10-17",
-    "Statement": [
+      "Version" : "2012-10-17",
+      "Statement" : [
         {
-            "Effect": "Allow",
-            "Action": "scheduler:*",
-            "Resource": "*"
+          "Effect" : "Allow",
+          "Action" : "scheduler:*",
+          "Resource" : "*"
         },
         {
-            "Effect": "Allow",
-            "Action": "iam:PassRole",
-            "Resource": "arn:aws:iam::*:role/*",
-            "Condition": {
-                "StringLike": {
-                    "iam:PassedToService": "scheduler.amazonaws.com"
-                }
+          "Effect" : "Allow",
+          "Action" : "iam:PassRole",
+          "Resource" : "arn:aws:iam::*:role/*",
+          "Condition" : {
+            "StringLike" : {
+              "iam:PassedToService" : "scheduler.amazonaws.com"
             }
+          }
         },
         {
-            "Sid": "AllowLambdaFunction",
-            "Effect": "Allow",
-            "Action": "lambda:InvokeFunction",
-            "Resource": "*"
+          "Sid" : "AllowLambdaFunction",
+          "Effect" : "Allow",
+          "Action" : "lambda:InvokeFunction",
+          "Resource" : "*"
         }
       ]
     }
@@ -31,6 +31,6 @@ resource "aws_iam_role" "eventbridge_scheduler_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "eventbridge_scheduler_policy_attachment" {
-  role = aws_iam_role.eventbridge_scheduler_role.name
+  role       = aws_iam_role.eventbridge_scheduler_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEventBridgeSchedulerFullAccess"
 }
--- modules/rds-stop-start/lambdas/stop_schedule.tf
+++ modules/rds-stop-start/lambdas/stop_schedule.tf
@@ -32,32 +32,32 @@ resource "aws_scheduler_schedule" "weekends_schedule" {
 
     input = jsonencode({
       resources = [{
-        type = "rds_db_instance"
+        type       = "rds_db_instance"
         identifier = "api"
-      },
-      {
-        type = "rds_db_instance"
-        identifier = "frontend"
-      },{
-        type = "ecs_service"
-        identifier = "api"
-        desiredCount = 1
-      },{
-        type = "ecs_service"
-        identifier = "frontend"
-        desiredCount = 1
-      },{
-        type = "ecs_service"
-        identifier = "admin"
-        desiredCount = 1
-      },{
-        type = "ecs_service"
-        identifier = "ingestion_worker"
-        desiredCount = 1
-      },{
-        type = "ecs_service"
-        identifier = "default_worker"
-        desiredCount = 1
+        },
+        {
+          type       = "rds_db_instance"
+          identifier = "frontend"
+          }, {
+          type         = "ecs_service"
+          identifier   = "api"
+          desiredCount = 1
+          }, {
+          type         = "ecs_service"
+          identifier   = "frontend"
+          desiredCount = 1
+          }, {
+          type         = "ecs_service"
+          identifier   = "admin"
+          desiredCount = 1
+          }, {
+          type         = "ecs_service"
+          identifier   = "ingestion_worker"
+          desiredCount = 1
+          }, {
+          type         = "ecs_service"
+          identifier   = "default_worker"
+          desiredCount = 1
       }]
     })
   }
