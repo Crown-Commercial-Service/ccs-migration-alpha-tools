a69ab97090faf8b8e214c9cdf0c066a8c1b6f10a
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2023-11-15 14:25:55 +0000

    Fix jsonencode

--- modules/rds-stop-start/lambdas/main.tf
+++ modules/rds-stop-start/lambdas/main.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "eventbridge_scheduler_role" {
   name = "eventbridge-scheduler-role"
-  assume_role_policy = jsondecode(
+  assume_role_policy = jsonencode(
     {
       "Version" : "2012-10-17",
       "Statement" : [
