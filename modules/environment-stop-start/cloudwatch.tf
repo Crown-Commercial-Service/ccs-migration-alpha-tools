86f7d9159e4d655a5a3856dfcd2f9a2c9332d764
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2023-11-16 14:47:36 +0000

    fmt

--- modules/environment-stop-start/cloudwatch.tf
+++ modules/environment-stop-start/cloudwatch.tf
@@ -1,6 +1,6 @@
 module "cloudwatch" {
   source = "../../resource-groups/cloudwatch-log-group"
 
-  log_group_name = "/aws/lambda/environment-start-stop"
+  log_group_name     = "/aws/lambda/environment-start-stop"
   log_retention_days = 7
 }
