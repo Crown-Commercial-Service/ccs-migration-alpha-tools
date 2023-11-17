3e256831add38a70e4f34beade4d6dfb3825c2ed
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2023-11-17 09:05:36 +0000

    fmt

--- modules/environment-stop-start/zip.tf
+++ modules/environment-stop-start/zip.tf
@@ -3,6 +3,6 @@ data "archive_file" "start_stop" {
     local_file.resources
   ]
   type        = "zip"
-  source_dir = "${path.module}/lambdas"
+  source_dir  = "${path.module}/lambdas"
   output_path = "${path.module}/start_stop.zip"
 }
