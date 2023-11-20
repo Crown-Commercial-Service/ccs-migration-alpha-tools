a64a7ec6b7ca7534d2a75128b5ef56e7087f8bc6
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2023-11-20 11:17:46 +0000

    Add conditional logic to script

--- modules/environment-stop-start/lambdas/start_stop.py
+++ modules/environment-stop-start/lambdas/start_stop.py
@@ -25,18 +25,35 @@ def start():
   for resource in data['resources']:
     if resource['type'] == 'rds_db_instance':
       try:
-        response = rds.start_db_instance(DBInstanceIdentifier=resource['identifier'])
-        print(response)
+        response = rds.describe_db_instances(DBInstanceIdentifier=resource['identifier'])
+        db_instance = response['DBInstances'][0]
+        status = db_instance['DBInstanceStatus']
+
+        if status == 'available':
+          print(f"RDS instance '{resource['identifier']}' is already started")
+        else:
+          response = rds.start_db_instance(DBInstanceIdentifier=resource['identifier'])
+          print(response)
       except Exception as e:
         return f"Error starting RDS instance: {str(e)}"
     elif resource['type'] == 'ecs_service':
       try:
         response = ecs.update_service(
           cluster = resource['cluster_name'],
-          service = resource['service_name'],
-          desiredCount = resource['desiredCount']
+          services = [resource['service_name']]
         )
-        print(response)
+        service = response['services'][0]
+        current_count = service['desiredCount']
+
+        if current_count == resource['desiredCount']:
+          print(f"ECS service '{resource['service_name']}' is already at the desired count of {current_count}.")
+        else:
+          update_response = ecs.update_service(
+            cluster = resource['cluster_name'],
+            service = resource['service_name'],
+            desiredCount = resource['desiredCount']
+          )
+          print(update_response)
       except Exception as e:
         return f"Error starting ECS service: {str(e)}"
 
@@ -51,18 +68,36 @@ def stop():
   for resource in data['resources']:
     if resource['type'] == 'rds_db_instance':
       try:
-        response = rds.stop_db_instance(DBInstanceIdentifier=resource['identifier'])
-        print(response)
+        response = rds.describe_db_instance(DBInstanceIdentifier=resource['identifier'])
+        db_instance = response['DBInstances'][0]
+        status = db_instance['DBInstanceStatus']
+
+        if status == 'stopped':
+          print(f"RDS instance '{resource['identifier']}' is already stopped.")
+        else:
+          stop_response = rds.stop_db_instance(DBInstanceIdentifier=resource['identifier'])
+          print(stop_response)
       except Exception as e:
         return f"Error stopping RDS instance: {str(e)}"
+
     elif resource['type'] == 'ecs_service':
       try:
         response = ecs.update_service(
           cluster = resource['cluster_name'],
           service = resource['service_name'],
-          desiredCount = 0
         )
-        print(response)
+        service = response['services'][0]
+        current_count = service['desiredCount']
+
+        if current_count == 0:
+          print(f"ECS service '{resource['service_name']}' is already stopped.")
+        else:
+          update_response = ecs.update_service(
+            cluster = resource['cluster_name'],
+            service = resource['service_name'],
+            desiredCount = 0
+          )
+          print(update_response)
       except Exception as e:
         return f"Error stopping ECS service: {str(e)}"
 
