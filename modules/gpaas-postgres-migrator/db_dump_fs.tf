3b77ddfd0559ac90931f5ce46e1026a0a939861d
muhammad9970 <muhammad.ahmad@crowncommercial.gov.uk>
2023-11-23 16:05:43 +0000

    update the depends on block

--- modules/gpaas-postgres-migrator/db_dump_fs.tf
+++ modules/gpaas-postgres-migrator/db_dump_fs.tf
@@ -63,7 +63,8 @@ EOF
 
 resource "aws_efs_mount_target" "db_dump" {
   depends_on = [
-    module.tailspend_full.module.vpc.aws_subnet.application
+    module.tailspend_full.module.vpc.aws_subnet.application["a"],
+    module.tailspend_full.module.vpc.aws_subnet.application["b"]
   ]
   for_each        = var.efs_subnet_ids
   file_system_id  = aws_efs_file_system.db_dump.id
