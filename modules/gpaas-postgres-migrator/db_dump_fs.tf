0b2adada4e07d037ab226ae6c1fd4a385c4e5ab8
muhammad9970 <muhammad.ahmad@crowncommercial.gov.uk>
2023-11-23 16:15:48 +0000

    updats

--- modules/gpaas-postgres-migrator/db_dump_fs.tf
+++ modules/gpaas-postgres-migrator/db_dump_fs.tf
@@ -63,7 +63,7 @@ EOF
 
 resource "aws_efs_mount_target" "db_dump" {
   depends_on = [
-    module.vpc.aws_subnet.application
+    module.vpc.subnets
   ]
   for_each        = var.efs_subnet_ids
   file_system_id  = aws_efs_file_system.db_dump.id
