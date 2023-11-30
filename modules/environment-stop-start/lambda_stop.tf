d19d856494e919bd51b29b6c30027f96b31d1f75
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2023-11-30 16:42:58 +0000

    Add permission to read items from DynamoDB

--- modules/environment-stop-start/lambda_stop.tf
+++ modules/environment-stop-start/lambda_stop.tf
@@ -31,6 +31,7 @@ resource "aws_iam_role_policy" "stop" {
     Statement = [
       {
         Action = [
+          "dynamodb:GetItem",
           "rds:DescribeDBInstances",
           "rds:StopDBInstance",
           "ecs:UpdateService",
