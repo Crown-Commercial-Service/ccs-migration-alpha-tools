b877696b7058da09c1f0b904aa0c3d9bf356d07f
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2023-12-01 08:52:13 +0000

    Create dynamo table

--- modules/environment-stop-start/resources.tf
+++ modules/environment-stop-start/resources.tf
@@ -1,4 +1,18 @@
-resource "local_file" "resources" {
-  content  = jsonencode(var.resources)
-  filename = "${path.module}/lambdas/resources.json"
+resource "aws_dynamodb_table" "start_stop" {
+  name     = "start-stop"
+
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table_item" "resources" {
+  table_name = aws_dynamodb_table.start_stop.name
+  hash_key   = aws_dynamodb_table.start_stop.hash_key
+
+  item = jsonencode(var.resources)
 }
