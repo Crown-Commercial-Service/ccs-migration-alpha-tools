d40782cba4cd885fee5fef838e130529e8c56d3b
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2024-01-20 21:10:14 +0000

    Add IAM

---
+++ modules/postgres-restore/iam.tf
@@ -0,0 +1,29 @@
+data "aws_iam_policy_document" "download_task" {
+  version = "2012-10-17"
+
+  statement {
+    sid = "AllowS3Download"
+
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListObjects"
+    ]
+
+    resources = [
+      "digitalmarketplace-database-backups-nft",
+      "digitalmarketplace-database-backups-nft/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "download_task" {
+  name = "postgres-s3-restore"
+  policy = data.aws_iam_policy_document.download_task.json
+}
+
+resource "aws_iam_role_policy_attachment" "download_task" {
+  role       = "pg_restore_api_download-ecs-task"
+  policy_arn = aws_iam_policy.download_task.arn
+}
