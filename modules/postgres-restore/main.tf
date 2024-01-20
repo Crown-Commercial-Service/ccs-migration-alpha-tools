4327d9fcc377a026defe2dcb93d67f9c5589cd65
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2024-01-20 23:15:17 +0000

    Update path

--- modules/postgres-restore/main.tf
+++ modules/postgres-restore/main.tf
@@ -57,7 +57,7 @@ resource "aws_sfn_state_machine" "perform_restore" {
               "Environment": [
                 {
                   "Name": "DUMP_FILENAME",
-                  "Value": "/mnt/efs0/${var.restore_name}.dir"
+                  "Value": "/mnt/efs0/nft-202401191508/"
                 }
               ]
             }
