c0de600e6c5e2170108e5764548645765279dbb7
Andrew Hemming <drewhemm@users.noreply.github.com>
2024-01-20 18:45:02 +0000

    Update download_sg.tf

--- modules/postgres-restore/download_sg.tf
+++ modules/postgres-restore/download_sg.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "migrate_download_task" {
+resource "aws_security_group" "download_task" {
   name        = "${var.resource_name_prefixes.normal}:PGRESTORE:${upper(var.restore_name)}:ECSTASK:DOWNLOAD"
   description = "Restore Download task"
   vpc_id      = var.vpc_id
@@ -13,7 +13,7 @@ resource "aws_security_group_rule" "migrate_download_task_https_out_anywhere" {
   description       = "Allow https out from download task to anywhere"
   from_port         = 443
   protocol          = "tcp"
-  security_group_id = aws_security_group.migrate_download_task.id
+  security_group_id = aws_security_group.download_task.id
   to_port           = 443
   type              = "egress"
 }
