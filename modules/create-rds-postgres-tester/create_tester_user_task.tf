ecd60e42f86fe533a91c8bd3410c244b34759b7a
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2024-04-02 18:31:28 +0000

    Update comment

--- modules/create-rds-postgres-tester/create_tester_user_task.tf
+++ modules/create-rds-postgres-tester/create_tester_user_task.tf
@@ -12,7 +12,7 @@ module "create_tester_user" {
       image                 = var.postgres_docker_image
       memory                = var.create_tester_user_task_memory
       mounts                = []
-      # N.B. $DUMP_FILENAME is injected by the Step Function task
+      # Requires use of an Alpine-based image, to install the AWS CLI to fetch the SQL from SSM
       override_command = [
         "sh", "-c",
         "apk update && apk add aws-cli && aws ssm get-parameter --name ${var.db_name}-postgres-create-tester-user-sql --query 'Parameter.Value' --output text > /tmp/create_tester_user.sql && psql -d $DB_CONNECTION_URL -f /tmp/create_tester_user.sql"
