bdec72e94a45365b76a822a88ebccf5a7d5fb868
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2024-04-02 17:43:01 +0000

    Add output for logs policy json

--- modules/create-rds-postgres-tester/outputs.tf
+++ modules/create-rds-postgres-tester/outputs.tf
@@ -2,3 +2,8 @@ output "pass_task_role_policy_document_json" {
   description = "JSON describing an IAM policy which allows passage of the task role"
   value       = module.create_tester_user.pass_task_role_policy_document_json
 }
+
+output "write_task_logs_policy_document_json" {
+  description = "JSON describing an IAM policy which allows the task's log streams to be written"
+  value       = module.create_tester_user.write_log_group_policy_document_json
+}
