06391adc17f0a4388c7677cac8ed5e3ef4467443
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2024-04-02 18:01:02 +0000

    Fix the output

--- modules/create-rds-postgres-tester/outputs.tf
+++ modules/create-rds-postgres-tester/outputs.tf
@@ -5,5 +5,5 @@ output "pass_task_role_policy_document_json" {
 
 output "write_task_logs_policy_document_json" {
   description = "JSON describing an IAM policy which allows the task's log streams to be written"
-  value       = module.create_tester_user.write_log_group_policy_document_json
+  value       = module.create_tester_user.write_task_logs_policy_document_json
 }
