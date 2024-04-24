c2a8089374b864a92711503e445f4d05464993be
Matt Brock <brock@cetre.co.uk>
2024-04-24 17:10:22 +0100

    Added variables to validation section

--- validation/opensearch-domain.tf
+++ validation/opensearch-domain.tf
@@ -2,6 +2,7 @@ module "opensearch_domain" {
   source = "../resource-groups/opensearch-domain"
 
   domain_name         = "domain"
+
   ebs_volume_size_gib = 10
   engine_version      = "OpenSearch_1.4"
   instance_type       = "m1.medium.search"
@@ -12,4 +13,12 @@ module "opensearch_domain" {
   }
   subnet_ids = ["subnet-123", "subnet-456"]
   vpc_id     = "vpc-12345"
+  enable_search_slow_logs = false
+  enable_index_slow_logs = false
+  enable_error_logs = false
+  enable_audit_logs = false
+  log_group_name_search_slow_logs = "opensearch-search-slow"
+  log_group_name_index_slow_logs = "opensearch-index-slow"
+  log_group_name_error_logs = "opensearch-error"
+  log_group_name_audit_logs = "opensearch-audit"
 }
