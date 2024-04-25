b7c9f6014e0d11b3a53cf9607aec8160c510d45d
Matt Brock <brock@cetre.co.uk>
2024-04-25 12:11:24 +0100

    Added log publishing policy

--- resource-groups/opensearch-domain/cloudwatch_logs.tf
+++ resource-groups/opensearch-domain/cloudwatch_logs.tf
@@ -17,3 +17,27 @@ module "audit_log_group" {
   source         = "../../resource-groups/cloudwatch-log-group"
   log_group_name = var.log_group_name_audit_logs
 }
+
+data "aws_iam_policy_document" "opensearch_cloudwatch_policy" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["es.amazonaws.com"]
+    }
+
+    actions = [
+      "logs:PutLogEvents",
+      "logs:PutLogEventsBatch",
+      "logs:CreateLogStream",
+    ]
+
+    resources = ["arn:aws:logs:*"]
+  }
+}
+
+resource "aws_cloudwatch_log_resource_policy" "opensearch_cloudwatch_policy" {
+  policy_name     = "opensearch_cloudwatch_policy"
+  policy_document = data.aws_iam_policy_document.opensearch_cloudwatch_policy.json
+}
