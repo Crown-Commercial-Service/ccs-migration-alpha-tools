d3160802ef643569d6857384f9d19c39f2fddd3f
Matt Brock <brock@cetre.co.uk>
2024-04-25 12:28:43 +0100

    Updated policy name format

--- resource-groups/opensearch-domain/cloudwatch_logs.tf
+++ resource-groups/opensearch-domain/cloudwatch_logs.tf
@@ -18,7 +18,7 @@ module "audit_log_group" {
   log_group_name = var.log_group_name_audit_logs
 }
 
-data "aws_iam_policy_document" "opensearch_cloudwatch_policy" {
+data "aws_iam_policy_document" "opensearch-log-publishing-policy" {
   statement {
     effect = "Allow"
 
@@ -37,7 +37,7 @@ data "aws_iam_policy_document" "opensearch_cloudwatch_policy" {
   }
 }
 
-resource "aws_cloudwatch_log_resource_policy" "opensearch_cloudwatch_policy" {
-  policy_name     = "opensearch_cloudwatch_policy"
-  policy_document = data.aws_iam_policy_document.opensearch_cloudwatch_policy.json
+resource "aws_cloudwatch_log_resource_policy" "opensearch-log-publishing-policy" {
+  policy_name     = "opensearch-log-publishing-policy"
+  policy_document = data.aws_iam_policy_document.opensearch-log-publishing-policy.json
 }
