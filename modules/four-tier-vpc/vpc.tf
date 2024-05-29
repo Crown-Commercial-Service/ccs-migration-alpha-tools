8a295e12de7c83b1454cdbe6e93c6ea684194f35
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2024-05-29 14:11:15 +0000

    Enable ECS Service Connect

--- modules/four-tier-vpc/vpc.tf
+++ modules/four-tier-vpc/vpc.tf
@@ -1,7 +1,8 @@
 resource "aws_vpc" "vpc" {
-  cidr_block           = var.vpc_cidr_block
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+  assign_generated_ipv6_cidr_block = true
+  cidr_block                       = var.vpc_cidr_block
+  enable_dns_hostnames             = true
+  enable_dns_support               = true
 
   tags = {
     Name = "${var.resource_name_prefixes.normal}:VPC"
