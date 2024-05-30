005255cfe2c9987f7f6cb0f32a3ecbf319e6a815
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2024-05-30 10:56:32 +0000

    Use /64 for IPv6 CIDR

--- modules/four-tier-vpc/calculate_cidr_blocks.tf
+++ modules/four-tier-vpc/calculate_cidr_blocks.tf
@@ -20,20 +20,20 @@ locals {
 
   ipv6_subnet_cidr_blocks = {
     "application" = {
-      "a" = cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 3, 4),
-      "b" = cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 3, 5),
+      "a" = cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 8, 4),
+      "b" = cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 8, 5),
     }
     # "database" = {
-    #   "a" = cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 3, 6),
-    #   "b" = cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 3, 7),
+    #   "a" = cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 8, 6),
+    #   "b" = cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 8, 7),
     # }
     # "public" = {
-    #   "a" = cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 3, 0),
-    #   "b" = cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 3, 1),
+    #   "a" = cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 8, 0),
+    #   "b" = cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 8, 1),
     # }
     "web" = {
-      "a" = cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 3, 2),
-      "b" = cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 3, 3),
+      "a" = cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 8, 2),
+      "b" = cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 8, 3),
     }
   }
 }
