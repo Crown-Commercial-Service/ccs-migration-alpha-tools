4e16eb5de5b6dac63c490071321e758a06c1993a
ghill95 <george.hill@crowncommercial.gov.uk>
2024-06-18 09:46:27 +0100

    Add output for bucket domain name

--- resource-groups/private-s3-bucket/outputs.tf
+++ resource-groups/private-s3-bucket/outputs.tf
@@ -1,3 +1,8 @@
+output "bucket_domain_name" {
+  description = "Name of the created S3 bucket"
+  value       = aws_s3_bucket.bucket.bucket_domain_name
+}
+
 output "bucket_id" {
   description = "Full name of created bucket"
   value       = aws_s3_bucket.bucket.id
