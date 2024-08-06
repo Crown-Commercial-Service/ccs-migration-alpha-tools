67f85324fc9ebfd5477933c192f3bedb3d42c83b
ghill95 <george.hill@crowncommercial.gov.uk>
2024-08-06 10:24:18 +0100

    Add count.index on resources leveraging SNS Topic

--- resource-groups/rds-postgres/rds_event_subscription.tf
+++ resource-groups/rds-postgres/rds_event_subscription.tf
@@ -7,14 +7,14 @@ resource "aws_sns_topic" "rds_event_subscription_sns_topic" {
 resource "aws_sns_topic_subscription" "rds_event_subscription_sns_topic_subscription" {
   count     = var.rds_event_subscription_enabled != false ? 1 : 0
   endpoint  = var.rds_event_subscription_email_endpoint
-  protocol  = "EMAIL"
-  topic_arn = aws_sns_topic.rds_event_subscription_sns_topic.arn
+  protocol  = "email"
+  topic_arn = aws_sns_topic.rds_event_subscription_sns_topic[count.index].arn
 }
 
 resource "aws_db_event_subscription" "rds_event_subscription" {
   count     = var.rds_event_subscription_enabled != false ? 1 : 0
   name      = "${var.db_name}-event-subscription"
-  sns_topic = aws_sns_topic.rds_event_subscription_sns_topic.arn
+  sns_topic = aws_sns_topic.rds_event_subscription_sns_topic[count.index].arn
 
   source_type = "db-instance"
   source_ids  = [aws_db_instance.db.identifier]
