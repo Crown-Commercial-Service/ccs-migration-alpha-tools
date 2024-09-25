4b09652c1f9d32d3d1a7b4b84b81310035ecfce4
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2024-09-25 18:02:14 +0000

    Remove extra brackets

--- modules/postgres-etl/main.tf
+++ modules/postgres-etl/main.tf
@@ -3,29 +3,28 @@ resource "aws_sfn_state_machine" "compile_objects_to_migrate" {
   role_arn = aws_iam_role.rds_to_s3_sfn.arn
 
   definition = <<EOF
-  {
-    "Comment": "State machine to run ECS task for pg_dump",
-    "StartAt": "RunEcsTask",
-    "States": {
-      "RunEcsTask": {
-        "Type": "Task",
-        "Resource": "arn:aws:states:::ecs:runTask.sync",
-        "Parameters": {
-          "Cluster": "${var.ecs_cluster_arn}",
-          "LaunchType": "FARGATE",
-          "TaskDefinition": "${module.extract_task.task_definition_arn}",
-          "NetworkConfiguration": {
-            "AwsvpcConfiguration": {
-            "AssignPublicIp": "DISABLED",
-            "SecurityGroups.$": "States.Array('${aws_security_group.etl_extract_task.id}', '${aws_security_group.db_etl_fs_clients.id}', '${var.db_clients_security_group_id}')",
-            "Subnets": ${jsonencode(var.subnet_ids)}
-            }
+{
+  "Comment": "State machine to run ECS task for pg_dump",
+  "StartAt": "RunEcsTask",
+  "States": {
+    "RunEcsTask": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::ecs:runTask.sync",
+      "Parameters": {
+        "Cluster": "${var.ecs_cluster_arn}",
+        "LaunchType": "FARGATE",
+        "TaskDefinition": "${module.extract_task.task_definition_arn}",
+        "NetworkConfiguration": {
+          "AwsvpcConfiguration": {
+          "AssignPublicIp": "DISABLED",
+          "SecurityGroups.$": "States.Array('${aws_security_group.etl_extract_task.id}', '${aws_security_group.db_etl_fs_clients.id}', '${var.db_clients_security_group_id}')",
+          "Subnets": ${jsonencode(var.subnet_ids)}
           }
-        },
-        "End": true
-      }
+        }
+      },
+      "End": true
     }
-  })
+  }
 }
 EOF
 
