cb0816651409c9f501af1a01187ee8a53f11e417
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2024-09-30 14:59:55 +0000

    Add s3 bucket

---
+++ modules/postgres-etl/s3.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "postgres_etl" {
+  bucket = "${var.s3_bucket_name}-${var.environment_name}"
+
+  tags = {
+    Name        = var.s3_bucket_name
+    Environment = "pre-prod"
+  }
+}
