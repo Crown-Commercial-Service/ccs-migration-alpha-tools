f5c2164d11b8c0ce42175c30854c81db0246d1b7
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2024-10-03 14:38:32 +0000

    Add ECR policies

---
+++ modules/postgres-etl/ecr_policies.tf
@@ -0,0 +1,33 @@
+data "aws_iam_policy_document" "pull_all_repo_images" {
+  version = "2012-10-17"
+
+  statement {
+    sid = "AllowGetAuthorizationToken"
+
+    effect = "Allow"
+
+    actions = [
+      "ecr:GetAuthorizationToken"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid = "AllowLayerAndImageAccess"
+
+    effect = "Allow"
+
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+    ]
+
+    resources = [
+      "arn:aws:ecr:${var.aws_region}:${var.aws_account_id}:repository/*"
+    ]
+  }
+}
