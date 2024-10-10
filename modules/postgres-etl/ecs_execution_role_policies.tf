b5898c6e0b19c567f38c09f048a94ae666687079
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2024-10-10 09:18:35 +0000

    Add condition to skip for Production

--- modules/postgres-etl/ecs_execution_role_policies.tf
+++ modules/postgres-etl/ecs_execution_role_policies.tf
@@ -8,7 +8,8 @@ data "aws_iam_policy_document" "postgres_etl" {
     data.aws_iam_policy_document.etl_policy.json,
     data.aws_iam_policy_document.read_creds_ssm.json,
     module.extract_task.write_task_logs_policy_document_json,
-    module.load_task.write_task_logs_policy_document_json
+    # module.load_task.write_task_logs_policy_document_json,
+    var.environment_name == "production" ? null : module.load_task.write_task_logs_policy_document_json
   ]
 }
 
@@ -31,18 +32,24 @@ resource "aws_iam_role_policy" "ecs_exec_policy__postgres_etl_extract" {
 }
 
 resource "aws_iam_role_policy" "ecs_execution_role__postgres_etl_load" {
+  count = var.environment_name == "production" ? 0 : 1 # This policy is skipped in production
+
   name   = "pg-etl-policy-load"
   role   = var.ecs_load_execution_role.name
   policy = data.aws_iam_policy_document.postgres_etl.json
 }
 
 resource "aws_iam_role_policy" "bucket_access__postgres_etl_load" {
+  count = var.environment_name == "production" ? 0 : 1 # This policy is skipped in production
+
   name   = "bucket_access_load"
   role   = var.ecs_load_execution_role.name
   policy = data.aws_iam_policy_document.bucket_access.json
 }
 
 resource "aws_iam_role_policy" "ecs_exec_policy__postgres_etl_load" {
+  count = var.environment_name == "production" ? 0 : 1 # This policy is skipped in production
+
   name   = "ecs_exec_policy_load"
   role   = var.ecs_load_execution_role.name
   policy = data.aws_iam_policy_document.ecs_exec_policy.json
