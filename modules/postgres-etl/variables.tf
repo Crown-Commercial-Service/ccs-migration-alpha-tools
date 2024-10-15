49c72103d8f715a6b82c27303f8712d70bb1266b
Andrew Hemming <andrew.hemming@redrockconsulting.co.uk>
2024-10-15 09:29:45 +0000

    Back to object

--- modules/postgres-etl/variables.tf
+++ modules/postgres-etl/variables.tf
@@ -23,24 +23,20 @@ variable "ecs_cluster_arn" {
   description = "ARN of cluster into which tasks will be deployed"
 }
 
-variable "ecs_extract_execution_role_arn" {
-  type        = string
-  description = "ARN of the role which is assumed by the ECS execution processes"
+variable "ecs_extract_execution_role" {
+  type = object({
+    arn  = string
+    name = string
+  })
+  description = "Details of the role which is assumed by the ECS execution processes"
 }
 
-variable "ecs_load_execution_role_arn" {
-  type        = string
-  description = "ARN of the role which is assumed by the ECS execution processes"
-}
-
-variable "ecs_extract_execution_role_name" {
-  type = string
-  description = "Name of the role which is assumed by the ECS execution processes"
-}
-
-variable "ecs_load_execution_role_name" {
-  type = string
-  description = "Name of the role which is assumed by the ECS execution processes"
+variable "ecs_load_execution_role" {
+  type = object({
+    arn  = string
+    name = string
+  })
+  description = "Details of the role which is assumed by the ECS execution processes"
 }
 
 variable "efs_subnet_ids" {
