85290ceccd6fac9e35885253a3c4df59671e5416
muhammad9970 <88196037+muhammad9970@users.noreply.github.com>
2025-03-03 16:18:17 +0000

    Update count for better readability
    Boolean values are used here directly so if true the value will be 1 and if false it will be 0

--- resource-groups/rds-postgres/rds_event_subscription.tf
+++ resource-groups/rds-postgres/rds_event_subscription.tf
@@ -1,18 +1,18 @@
 resource "aws_sns_topic" "rds_event_subscription_sns_topic" {
-  count             = var.rds_event_subscription_enabled != false ? 1 : 0
+  count             = var.rds_event_subscription_enabled ? 1 : 0
   name              = "${var.db_name}-sns-topic"
   kms_master_key_id = "alias/aws/sns"
 }
 
 resource "aws_sns_topic_subscription" "rds_event_subscription_sns_topic_subscription" {
-  count     = var.rds_event_subscription_enabled != false ? 1 : 0
+  count     = var.rds_event_subscription_enabled ? 1 : 0
   endpoint  = var.rds_event_subscription_email_endpoint
   protocol  = "email"
   topic_arn = aws_sns_topic.rds_event_subscription_sns_topic[count.index].arn
 }
 
 resource "aws_db_event_subscription" "rds_event_subscription" {
-  count     = var.rds_event_subscription_enabled != false ? 1 : 0
+  count     = var.rds_event_subscription_enabled ? 1 : 0
   name      = "${var.db_name}-event-subscription"
   sns_topic = aws_sns_topic.rds_event_subscription_sns_topic[count.index].arn
 
