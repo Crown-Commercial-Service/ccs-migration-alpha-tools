22ca921d67c7ec56b6a4506d9581e1d8fa12b6d9
muhammad9970 <muhammad.ahmad@crowncommercial.gov.uk>
2025-06-18 09:05:47 +0000

    Add outputs

---
+++ modules/eks-cluster/outputs.tf
@@ -0,0 +1,16 @@
+output "endpoint" {
+  value = aws_eks_cluster.this.endpoint
+}
+output "ca" {
+  value = aws_eks_cluster.this.certificate_authority[0].data
+}
+output "cluster_security_group_id" {
+  value = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
+}
+
+output "oidc_provider" {
+  value = {
+    url               = aws_eks_cluster.this.identity[0].oidc[0].issuer
+    url_sans_protocol = replace(aws_eks_cluster.this.identity[0].oidc[0].issuer, "https://", "")
+  }
+}
