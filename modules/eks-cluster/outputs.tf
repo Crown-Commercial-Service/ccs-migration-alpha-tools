8ea52b8838715b2452cdd16103c38c508ce044d8
muhammad9970 <muhammad.ahmad@crowncommercial.gov.uk>
2025-06-18 09:07:06 +0000

    Alphabetical order

--- modules/eks-cluster/outputs.tf
+++ modules/eks-cluster/outputs.tf
@@ -1,11 +1,11 @@
-output "endpoint" {
-  value = aws_eks_cluster.this.endpoint
-}
 output "ca" {
   value = aws_eks_cluster.this.certificate_authority[0].data
 }
 output "cluster_security_group_id" {
   value = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
+}
+output "endpoint" {
+  value = aws_eks_cluster.this.endpoint
 }
 
 output "oidc_provider" {
