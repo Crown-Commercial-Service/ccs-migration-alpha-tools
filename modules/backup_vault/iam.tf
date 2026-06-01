8562d4b166839dddb43f37f0c1111eab39aebb80
Will Trust <will.trust@crowncommercial.gov.uk>
2026-06-01 11:06:17 +0100

    backup dropped eb policy

--- modules/backup_vault/iam.tf
+++ modules/backup_vault/iam.tf
@@ -9,12 +9,6 @@ resource "aws_iam_policy" "backup_kms_access" {
   policy = data.aws_iam_policy_document.backup_kms_access.json
 }
 
-resource "aws_iam_policy" "eventbridge_forwarder_policy" {
-  name        = "EventBridgeCrossAccountForwarderPolicy"
-  description = "Allows forwarding events to the staging account"
-  policy      = data.aws_iam_policy_document.eventbridge_forwarder_policy.json
-}
-
 resource "aws_iam_role_policy_attachment" "backup_kms_access" {
   role       = aws_iam_role.backup_role.name
   policy_arn = aws_iam_policy.backup_kms_access.arn
