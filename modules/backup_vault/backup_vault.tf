ab135e38bf9b7394ccd51317b969555919a216ef
Will Trust <will.trust@crowncommercial.gov.uk>
2026-06-05 10:56:52 +0100

    backup remove lifecycle

--- modules/backup_vault/backup_vault.tf
+++ modules/backup_vault/backup_vault.tf
@@ -1,12 +1,6 @@
 resource "aws_backup_vault" "backup_vault" {
   name        = "backup_vault"
   kms_key_arn = "arn:aws:kms:${local.primary_region}:${var.backup_environment_id}:key/${var.backup_kms_key_id}"
-
-  lifecycle {
-    ignore_changes = [
-      recovery_points,
-    ]
-  }
 }
 
 resource "aws_backup_vault" "backup_vault_transfer" {
@@ -14,12 +8,6 @@ resource "aws_backup_vault" "backup_vault_transfer" {
   provider    = aws.secondary_region
   name        = "backup_vault_transfer"
   kms_key_arn = "arn:aws:kms:${local.secondary_region}:${var.backup_environment_id}:key/${var.backup_kms_key_id}"
-
-  lifecycle {
-    ignore_changes = [
-      recovery_points,
-    ]
-  }
 }
 
 resource "aws_backup_vault_policy" "backup_vault_policy" {
